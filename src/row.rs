b7906e727eb7e69cc0a0efe869441ba98a6a8990
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2022-05-18 06:46:24 +0000

    feat(syntax-highlight) improve numbers highlighting

--- src/row.rs
+++ src/row.rs
@@ -223,6 +223,7 @@ impl Row {
             }
         }
 
+        let mut prev_is_separator = true;
         let mut index = 0;
         while let Some(c) = chars.get(index) {
             if let Some(word) = word {
@@ -239,11 +240,24 @@ impl Row {
                 }
             }
 
-            if c.is_ascii_digit() {
+            let previous_highlight = if index > 0 {
+                highlighting
+                    .get(index.saturating_sub(1))
+                    .unwrap_or(&highlighting::Type::None)
+            } else {
+                &highlighting::Type::None
+            };
+
+            if (c.is_ascii_digit()
+                && (prev_is_separator || previous_highlight == &highlighting::Type::Number))
+                || (c == &'.' && previous_highlight == &highlighting::Type::Number)
+            {
                 highlighting.push(highlighting::Type::Number);
             } else {
                 highlighting.push(highlighting::Type::None);
             }
+
+            prev_is_separator = c.is_ascii_punctuation() || c.is_ascii_whitespace();
             index = index.saturating_add(1);
         }
 
